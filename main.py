f5bca25d17faa69fcdd407ce0d6a9c5b8dcc2d0d
Vedat Gül <124357663+Vedatgul@users.noreply.github.com>
2023-05-12 15:42:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,8 @@
 import streamlit as st
 st.title('hello')
 st.sidebar.header("New customer information:")
+country = st.sidebar.selectbox("Country of new user: ", {"BRA", "TUR", "USA", "CAN", "DEU","FRA"})
+phone_type = st.sidebar.selectbox("Operating System of new user: ", {"Android", "IOS"})
+gender = st.sidebar.selectbox("Gender of new user: ", {"Female", "Male"})
+age = st.sidebar.number_input("Age of new user",value=18, step=1)
 
