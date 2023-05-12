82c39fac2e2a160219b7e418584f5ca617f7c9e5
Vedat Gül <124357663+Vedatgul@users.noreply.github.com>
2023-05-12 16:26:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 import streamlit as st
 st.title('hello')
 st.sidebar.header("New user information:")
-gender = st.sidebar.selectbox("Gender of new user: ", {"Female", "Male"})
+st.radio('Gender of new user:', options=['Female', 'Male'], horizontal=True)
+
 Pregnancies = st.sidebar.number_input("Pregnanciesof",value=17, step=1)
 Glucose = st.sidebar.number_input("Glucose",value=199, step=0)
 BloodPressure = st.sidebar.number_input("BloodPressure",value=122, step=0)
@@ -15,28 +16,7 @@ DiabetesPedigreeFunction = st.sidebar.number_input("DiabetesPedigreeFunction",va
 Age = st.sidebar.number_input("Age",value=67.1, step=0)
 
 
-import streamlit as st
-import base64
 
-main_bg = "x.jpg"
-main_bg_ext = "jpg"
-
-side_bg = "x.jpg"
-side_bg_ext = "jpg"
-
-st.markdown(
-    f"""
-    <style>
-    .reportview-container {{
-        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
-    }}
-   .sidebar .sidebar-content {{
-        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 
 
