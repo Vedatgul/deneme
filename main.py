bfbf2d91841834c9c03aea6f7cbd7df3d94a0fb2
Vedat Gül <124357663+Vedatgul@users.noreply.github.com>
2023-05-12 16:45:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import streamlit as st
 st.title('hello')
 
 st.sidebar.header("New user information:")
-
-
-age = st.slider('How old are you?', 0, 130, 25)
-
-
-
+st.sidebar.radio('Gender of new user:', options=['Female', 'Male'],  horizontal=True)
+Pregnancies = st.sidebar.number_input("Pregnanciesof",value=17, step=1)
+age = st.slider('How old are you?', 0, 90, 25)
+Glucose = st.slider('Glucose', 0, 199, 50)
+BloodPressure = st.slider('BloodPressure', 0, 122, 45)
+DiabetesPedigreeFunction = st.slider('DiabetesPedigreeFunction', 0, 122, 45)
+SkinThickness = st.sidebar.number_input("SkinThickness",value=99, step=0)
+Insulin = st.sidebar.number_input("Insulin",value=846, step=0)
+BMI = st.sidebar.number_input("BMI",value=67, step=0)
 
 
 
