b8af308970b98525c74fbf077e249ec493888628
Vedat Gül <124357663+Vedatgul@users.noreply.github.com>
2023-05-12 16:47:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 
 # Mission 1
 import streamlit as st
-st.title('hello')
+st.title('Diabetes Prediction')
 
 st.sidebar.header("New user information:")
 st.sidebar.radio('Gender of new user:', options=['Female', 'Male'],  horizontal=True)
-Pregnancies = st.sidebar.number_input("Pregnanciesof",value=17, step=1)
+Pregnancies = st.sidebar.number_input("Pregnancies",value=17, step=1)
 age = st.slider('How old are you?', 0, 90, 25)
 Glucose = st.slider('Glucose', 0, 199, 50)
 BloodPressure = st.slider('BloodPressure', 0, 122, 45)
@@ -14,6 +14,7 @@ DiabetesPedigreeFunction = st.slider('DiabetesPedigreeFunction', 0, 122, 45)
 SkinThickness = st.sidebar.number_input("SkinThickness",value=99, step=0)
 Insulin = st.sidebar.number_input("Insulin",value=846, step=0)
 BMI = st.sidebar.number_input("BMI",value=67, step=0)
+st.set_page_config(layout="wide")
 
 
 
