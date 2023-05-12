653a0e5f9e0dfd13bb3d46204d393b607f6c87d2
Vedat Gül <124357663+Vedatgul@users.noreply.github.com>
2023-05-12 16:18:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,20 +16,28 @@ Age = st.sidebar.number_input("Age",value=67.1, step=0)
 
 
 
-st.sidebar.markdown("Side")
+import streamlit as st
+import base64
+
+main_bg = "sample.jpg"
+main_bg_ext = "jpg"
+
+side_bg = "sample.jpg"
+side_bg_ext = "jpg"
+
 st.markdown(
-    """
-    <style> 
-    .reportview-container {
-        background : url("x.jpeg")
-       }
-       .sidebar.sidebar-content {
-          background: url("x.jpeg")
-       }
-         </style> 
-         """,
-         unsafe_allow_html = True
-   )
+    f"""
+    <style>
+    .reportview-container {{
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+    }}
+   .sidebar .sidebar-content {{
+        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 
 
