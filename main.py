52a04a6ef03488d65895c7b5dfa6683b5f9438e4
Vedat Gül <124357663+Vedatgul@users.noreply.github.com>
2023-05-12 16:51:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ left, right = st.columns(2)
 with left:     
   st.title('Diabetes Prediction')
 
-st.bar.header("New user information:")
-st.bar.radio('Gender of new user:', options=['Female', 'Male'],  horizontal=True)
-Pregnancies = st.bar.number_input("Pregnancies",value=17, step=1)
+st.sidebar.header("New user information:")
+st.sidebar.radio('Gender of new user:', options=['Female', 'Male'],  horizontal=True)
+Pregnancies = st.number_input("Pregnancies",value=17, step=1)
 age = st.slider('How old are you?', 0, 90, 25)
 Glucose = st.slider('Glucose', 0, 199, 50)
 BloodPressure = st.slider('BloodPressure', 0, 122, 45)
 DiabetesPedigreeFunction = st.slider('DiabetesPedigreeFunction', 0, 122, 45)
 with right:     
-SkinThickness = st.bar.number_input("SkinThickness",value=99, step=0)
-Insulin = st.bar.number_input("Insulin",value=846, step=0)
-BMI = st.bar.number_input("BMI",value=67, step=0)
+SkinThickness = st.number_input("SkinThickness",value=99, step=0)
+Insulin = st.number_input("Insulin",value=846, step=0)
+BMI = st.number_input("BMI",value=67, step=0)
 st.set_page_config(layout="wide")
 
 
