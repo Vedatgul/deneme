36533e21f76f43311ecfbaee186a1c407c8d97a7
Vedat Gül <124357663+Vedatgul@users.noreply.github.com>
2023-05-12 16:36:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@
 # Mission 1
 import streamlit as st
 st.title('hello')
+
+
 st.sidebar.header("New user information:")
 st.sidebar.radio('Gender of new user:', options=['Female', 'Male'],  horizontal=True)
-
 Pregnancies = st.sidebar.number_input("Pregnanciesof",value=17, step=1)
 Glucose = st.sidebar.number_input("Glucose",value=199, step=0)
 BloodPressure = st.sidebar.number_input("BloodPressure",value=122, step=0)
@@ -15,7 +16,6 @@ BMI = st.sidebar.number_input("BMI",value=67, step=0)
 DiabetesPedigreeFunction = st.sidebar.number_input("DiabetesPedigreeFunction",value=2, step=0) 
 Age = st.sidebar.number_input("Age",value=67.1, step=0)
 
-st.set_page_config(layout="wide")
 
 
 
